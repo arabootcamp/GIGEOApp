e41075cfb1e0f3d64f96cec8ce404f248e1fc7bb
aracelly <aracellybootcamp@gmail.com>
2021-06-26 15:09:16 -0400

    se agrega diseño y js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
-$(function() {
+$(function () {
   // Handler for .ready() called.
+  const breakpoint=575.98;//767.98;
+
+  //settting load
+  window.addEventListener("load", function () {
+    if ($(window).width() <= breakpoint) {
+      $('.navbar').addClass('position-relative');
+      $('.navbar__menu').addClass('position-absolute top-100 end-0 bg-light ');
+    }
+    $('.navbar__menu').removeClass('invisible');
+    $('.navbar__menu').hide();
+  });
+
+  //click bars
+  $('.button--bars').click(function () {
+    $('.navbar__menu').toggle();
+  });
+
+  //position to lower the menu
+  $(window).resize(function () {
+    if ($(window).width() <= breakpoint) {
+      $('.navbar').addClass('position-relative');
+      $('.navbar__menu').addClass('position-absolute top-100 end-0 bg-light ');
+    } else {
+      $('.navbar').removeClass('position-relative');
+      $('.navbar__menu').removeClass('position-absolute top-100 end-0 bg-light ');
+    }
+  });
 
 });
